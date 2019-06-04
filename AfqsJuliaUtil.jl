76dbde10618ee48f2ad40c470201c7a3d85c4f87
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2019-06-03 23:29:37 -0700

    made a version that was an eval function

--- AfqsJuliaUtil.jl
+++ AfqsJuliaUtil.jl
@@ -73,28 +73,28 @@ macro polynomial_function(len,order)
     terms = polynomial_indices(len,order)
     multiply_terms(indcs) = Expr(:call,*,[:(x[$i]) for i in indcs]...)
     Q = quote
-        function poly(x::Array{NumT}) where {NumT <: Number}
+        @inline function poly(x::Array{NumT}) where {NumT <: Number}
             @inbounds begin
                 NumT[$(map(multiply_terms,terms)...)]
             end
         end
-        function poly(x::Array{Array{NumT,1},1}) where {NumT <: Number}
+        @inline function poly(x::Array{Array{NumT,1},1}) where {NumT <: Number}
             poly.(x)
         end
-        function poly(x::Array{NumT,2}) where {NumT <: Number}
+        @inline function poly(x::Array{NumT,2}) where {NumT <: Number}
             #hcat(poly.(x)...)
             hcat([poly(y) for y in x]...)
         end
     end 
     if len==1 && order==1
         Q *= quote
-            function poly(x::NumT) where {NumT <:Number}
+            @inline function poly(x::NumT) where {NumT <:Number}
                 poly([x])[1]
             end
         end
     elseif len==1
          Q *= quote
-            function poly(x::NumT) where {NumT <:Number}
+            @inline function poly(x::NumT) where {NumT <:Number}
                 poly([x])
             end
         end 
@@ -103,5 +103,39 @@ macro polynomial_function(len,order)
     Q
 end
 
-
+function polynomial_function(len,order)
+    terms = polynomial_indices(len,order)
+    multiply_terms(indcs) = Expr(:call,*,[:(x[$i]) for i in indcs]...)
+    poly = gensym("poly")
+    Q = quote
+        @inline function $poly(x::Array{NumT}) where {NumT <: Number}
+            @inbounds begin
+                NumT[$(map(multiply_terms,terms)...)]
+            end
+        end
+        @inline function $poly(x::Array{Array{NumT,1},1}) where {NumT <: Number}
+            $poly.(x)
+        end
+        @inline function $poly(x::Array{NumT,2}) where {NumT <: Number}
+            #hcat(poly.(x)...)
+            hcat([$poly(y) for y in x]...)
+        end
+    end 
+    if len==1 && order==1
+        Q *= quote
+            @inline function $poly(x::NumT) where {NumT <:Number}
+                $poly([x])[1]
+            end
+        end
+    elseif len==1
+         Q *= quote
+            @inline function $poly(x::NumT) where {NumT <:Number}
+                $poly([x])
+            end
+        end 
+    end
+    # println(rmlines(Q))
+    eval(Q)
+end
+    
 end
